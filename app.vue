f5437cf1703db9ded52dba0f15b939ddae5b1cbd
Tariq <tariq.nazar13@gmail.com>
2023-04-09 14:21:37 +0200

    fixed container centering to match prose width

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class ="absolute w-full bg-slate-50 font-mono p-5">
-    <div class="container max-w-3xl">
+  <div class ="absolute w-full min-h-full bg-slate-50 font-mono p-5">
+    <div class="container max-w-prose">
       <NavBar />
       <NuxtPage/>
     </div>
