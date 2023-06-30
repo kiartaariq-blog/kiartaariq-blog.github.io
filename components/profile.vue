1633cf82813ab956ba9f9a90492a875861155545
Xiar <54110012+Xiar-fatah@users.noreply.github.com>
2023-06-30 21:13:23 +0200

    Update profile.vue

--- components/profile.vue
+++ components/profile.vue
@@ -1,4 +1,12 @@
 <template>
+    <h1 class="text-3xl">'test'</h1>
+    <div>
+        <head>
+        <!-- Other meta tags and head content here -->
+        <meta name="google-site-verification" content="2koi5rkebY-RyuRXC71QRFhRAlOKft3MfX6eMdQAjwg" />
+        <!-- Other head content here -->
+        </head>
+    </div>
     <div class = "flex flex-row justify-items-stretch h-60">
        <div class = "flex flex-col justify-items-stretch w-1/2 m-1 rounded-md">
             <div class = "h-1/2 m-1">
